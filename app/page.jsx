844b8a7e11e96ae3b16bf1b93d62f0049d4ad5c9
Keylan King <kingkey0101@outlook.com>
2025-06-12 13:36:27 -0500

    Finishing header setup w/ logo and functional buttons

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="pt-16">
       testing
       <br/>
       <Button variant={'destructive'}>Subscribe</Button>
