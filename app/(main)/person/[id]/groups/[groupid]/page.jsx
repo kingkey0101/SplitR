7c5243e9dc7f423b8156295fba721df0347eb99d
Keylan King <kingkey0101@outlook.com>
2025-06-23 23:42:45 -0500

    bug in localhost:3000/groups

--- app/(main)/person/[id]/groups/[groupid]/page.jsx
+++ app/(main)/person/[id]/groups/[groupid]/page.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const GroupPage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <div>GroupPage</div>;
+};
 
-export default GroupPage
+export default GroupPage;
